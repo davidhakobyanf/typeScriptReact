f2a50da10655f85ad634e08fd093c03318b0e96e
Sargis Abrahamyan <sargis.abrahamyan033@gmail.com>
2025-02-03 18:19:37 +0400

    first

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface = CardProps{
+  width?:string;
+  height?:string;
+}
+
+const Card = ({width,height}:CardProps) => {
+
+
+  return(
+    
+  );
+};
+export default Card;
